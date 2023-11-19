4ba26a528796eaac925a76cb70b8425a169f5cbd
Mattia <146854645+Mattia-Visentin@users.noreply.github.com>
2023-11-19 21:56:10 +0100

    Update

---
+++ Calcolatrice/CalcolatriceCodice.js
@@ -0,0 +1,197 @@
+let numero = Array("");
+
+let operazione = Array();
+
+function aggiungi(tasto) {
+
+    console.log(numero);
+
+    console.log(operazione);
+
+    let schermo = document.getElementById("risultato");
+
+    numero[operazione.length != 0 ? (operazione.length) : 0] += tasto;
+
+    schermo.innerHTML = numero[operazione.length != 0 ? (operazione.length) : 0];
+}
+
+function AC() {
+
+    operazione = []; numero = [""];
+
+    document.getElementById("risultato").innerHTML = "0";
+}
+
+
+function operaz(oper) {
+
+    operazione.push(oper);
+
+    numero.push("");
+}
+
+function segno() {
+
+    let schermo = document.getElementById("risultato");
+
+    numero[operazione.length != 0 ? (operazione.length) : 0] = parseFloat(numero[operazione.length != 0 ? (operazione.length) : 0] * -1);
+
+    schermo.innerHTML = numero[operazione.length != 0 ? (operazione.length) : 0];
+
+}
+
+function virgola() {
+
+    let schermo = document.getElementById("risultato");
+
+    if(!String(numero[operazione.length != 0 ? (operazione.length) : 0]).includes(".")) {
+
+        numero[operazione.length != 0 ? (operazione.length) : 0] += ".";
+
+        schermo.innerHTML = numero[operazione.length != 0 ? (operazione.length) : 0];
+    }
+}
+
+function perCento() {
+
+    numero[operazione.length != 0 ? (operazione.length - 1) : 0] = (parseFloat(numero[operazione.length != 0 ? (operazione.length - 1) : 0]) / 100);
+
+    visualizzaRisultato(numero[operazione.length != 0 ? (operazione.length - 1) : 0]);
+
+}
+
+function visualizzaRisultato(numero) {
+
+
+    let schermo = document.getElementById("risultato");
+
+    if(String(numero).includes('.')) { 
+
+        if(((String(numero).length - String(numero).indexOf(".")) - 1) > 3) {
+
+            numero = parseFloat(numero).toFixed(3);
+        }
+    }
+
+    schermo.innerHTML = numero;
+}
+
+function quadrato() {
+
+    let schermo = document.getElementById("risultato");
+
+    numero[operazione.length != 0 ? (operazione.length) : 0] = Math.pow(parseFloat(numero[operazione.length != 0 ? (operazione.length) : 0]), 2);
+
+    visualizzaRisultato(numero[operazione.length != 0 ? (operazione.length) : 0]);
+}
+
+function cubo() {
+
+    let schermo = document.getElementById("risultato");
+
+    numero[operazione.length != 0 ? (operazione.length) : 0] = Math.pow(parseFloat(numero[operazione.length != 0 ? (operazione.length) : 0]), 3);
+
+    visualizzaRisultato(numero[operazione.length != 0 ? (operazione.length) : 0]);
+}
+
+function radice() {
+
+    let schermo = document.getElementById("risultato");
+
+    numero[operazione.length != 0 ? (operazione.length - 1) : 0] = Math.sqrt(parseFloat(numero[operazione.length != 0 ? (operazione.length - 1) : 0]));
+
+    visualizzaRisultato(numero[operazione.length != 0 ? (operazione.length - 1) : 0]);
+}
+
+function radiceCubica() {
+
+    let schermo = document.getElementById("risultato");
+
+    numero[operazione.length != 0 ? (operazione.length - 1) : 0] = Math.cbrt(parseFloat(numero[operazione.length != 0 ? (operazione.length - 1) : 0]));
+
+    visualizzaRisultato(numero[operazione.length != 0 ? (operazione.length - 1) : 0]);
+}
+
+function uguale() {
+
+    let schermo = document.getElementById("risultato");
+
+    while(numero.length != 1) {
+
+        if(operazione.includes("moltiplicazione") || operazione.includes("divisione")) {
+
+            for(let i = 0; i < operazione.length; i++) {
+
+                switch(operazione[i]) {
+
+                case "moltiplicazione":
+
+                    numero[i] = (parseFloat(numero[i]) * parseFloat(numero[i + 1]));
+
+                    break;
+
+                case "divisione":
+
+                    numero[i] = ((parseFloat(numero[i]) / parseFloat(numero[i + 1]))).toFixed(3);
+
+                    break;
+
+                default:
+
+                    continue;
+                }
+
+                rimuovi(i + 1);
+        
+                rimuoviOper(i);
+            }
+
+        } else {
+
+            for(let i = 0; i < operazione.length; i++) {
+
+                switch(operazione[i]) {
+    
+                case "addizione":
+    
+                    numero[i] = (parseFloat(numero[i]) + parseFloat(numero[i + 1]));
+    
+                    break;
+    
+                case "differenza":
+    
+                    numero[i] = (parseFloat(numero[i]) - parseFloat(numero[i + 1]));
+    
+                    break;
+    
+                default:
+    
+                    continue;
+                }
+    
+                rimuovi(i + 1);
+        
+                rimuoviOper(i);
+            }
+        }
+    
+        schermo.innerHTML = numero[0];
+        //operazione = []; numero = [""];
+    }   
+}
+
+function rimuovi(indice) {
+
+    if (indice >= 0 && indice < numero.length) {
+
+        numero.splice(indice, 1)
+    }
+}
+
+function rimuoviOper(indice) {
+
+    if (indice >= 0 && indice < operazione.length) {
+        operazione.splice(indice, 1);
+        
+    }
+}
