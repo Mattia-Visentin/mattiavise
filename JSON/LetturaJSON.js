6301a95645cf192fad8c59f564416ce57669ea9b
Mattia <146854645+Mattia-Visentin@users.noreply.github.com>
2024-10-16 18:37:04 +0200

    Update
    Update file

--- JSON/LetturaJSON.js
+++ JSON/LetturaJSON.js
@@ -2,7 +2,7 @@ function caricaDati() {
 
     var file = new XMLHttpRequest();
 
-    file.open("GET", "Anagrafica.json", true);
+    file.open("GET", "https://raw.githubusercontent.com/Mattia-Visentin/mattiavise/refs/heads/main/JSON/Anagrafica.json", true);
 
     file.send();
 
@@ -12,7 +12,27 @@ function caricaDati() {
 
             var jsonDoc = JSON.parse(file.responseText);
 
-            document.getElementById("stringaJSON").innerHTML = jsonDoc.nomi[0];
+            mostraInfo(jsonDoc);
+
+            //document.getElementById("stringaJSON").innerHTML = jsonDoc.Nomi[0].Nome;
         }
+    }
+
+    document.getElementById("risForm").style.visibility = "visible"
+}
+
+function mostraInfo(ogg) {
+
+    for(var n = 0; n < 3; n++) {
+
+        let testo = document.createElement("text");
+
+        testo.classList.add("testoElenco");
+
+        testo.textContent = ogg.Nomi[n].Nome
+
+        document.getElementById("stringaJSON").appendChild(testo);
+
+        document.getElementById("stringaJSON").appendChild(document.createElement("br"));
     }
 }
