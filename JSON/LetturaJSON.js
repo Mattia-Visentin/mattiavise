2b106be15545847bb489306fb104d1a61cc9f6b9
Mattia <146854645+Mattia-Visentin@users.noreply.github.com>
2024-10-20 16:30:04 +0200

    Update
    Update file JSON

--- JSON/LetturaJSON.js
+++ JSON/LetturaJSON.js
@@ -1,38 +1,56 @@
 function caricaDati() {
 
     var file = new XMLHttpRequest();
-
+    
     file.open("GET", "https://raw.githubusercontent.com/Mattia-Visentin/mattiavise/refs/heads/main/JSON/Anagrafica.json", true);
-
+    
     file.send();
-
+    
     file.onreadystatechange = function() {
-
-        if(this.readyState == 4 && this.status == 200) {
-
+        
+        if (this.readyState == 4 && this.status == 200) {
+           
             var jsonDoc = JSON.parse(file.responseText);
-
+            
             mostraInfo(jsonDoc);
-
-            //document.getElementById("stringaJSON").innerHTML = jsonDoc.Nomi[0].Nome;
         }
-    }
+    };
 
-    document.getElementById("risForm").style.visibility = "visible"
+    document.getElementById("risForm").style.visibility = "visible";
 }
 
 function mostraInfo(ogg) {
+    
+    var tabella = document.createElement("table");
 
-    for(var n = 0; n < 3; n++) {
+    for (let i = 0; i < 3; i++) {
+        
+        var colonna = document.createElement("tr");
+       
+        var righe = document.createElement("td");
 
-        let testo = document.createElement("text");
+        righe.appendChild(document.createTextNode(ogg.Nomi[i].Nome + ", "));
+        
+        righe.appendChild(document.createTextNode(ogg.Cognomi[i].Cognome + ", "));
+        
+        righe.appendChild(document.createTextNode(ogg.Eta[i].Eta + ", "));
+        
+        righe.appendChild(document.createTextNode(ogg.Indirizzi[i].Indirizzo + ", "));
+        
+        righe.appendChild(document.createTextNode(ogg.Citta[i].Citta + ", "));
 
-        testo.classList.add("testoElenco");
+        righe.appendChild(document.createTextNode("Telefono1: " + ogg.Telefoni[0].Telefoni[i].Telefono1 + ", "));
+       
+        righe.appendChild(document.createTextNode("Telefono2: " + ogg.Telefoni[0].Telefoni[i].Telefono2 + ", "));
 
-        testo.textContent = ogg.Nomi[n].Nome
+        righe.appendChild(document.createTextNode("Sport1: " + ogg.Sport[0].Sport[i].Sport1 + ", "));
+        
+        righe.appendChild(document.createTextNode("Sport2: " + ogg.Sport[0].Sport[i].Sport2 + " "));
 
-        document.getElementById("stringaJSON").appendChild(testo);
-
-        document.getElementById("stringaJSON").appendChild(document.createElement("br"));
+        colonna.appendChild(righe);
+        
+        tabella.appendChild(colonna);
     }
+
+    document.getElementById("stringaJSON").appendChild(tabella);
 }
