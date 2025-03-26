3964d965b71d0076fc1df96c2908334a4d8fe6a0
Mattia <146854645+Mattia-Visentin@users.noreply.github.com>
2025-03-26 16:27:50 +0100

    Update Server.js

--- NodeJS/Server.js
+++ NodeJS/Server.js
@@ -2,11 +2,50 @@ const http = require('http');
 
 const server = http.createServer((req, res) => {
 
-    res.statusCode = 200;
+    res.writeHead(200, { 'Content-Type': 'text/html' });
 
-    res.setHeader('Content-Type', 'text/plain');
+    res.end(`
 
-    res.end('Hello, World!\n');
+        <!DOCTYPE html>
+
+        <html lang="it">
+
+            <head>
+
+                <title>Inserisci testo</title>
+
+                <style>
+                    body { font-family: Arial; text-align: center; margin-top: 30px; }
+                    input, button { padding: 10px; margin: 5px; border-radius: 8px; border: none}
+                    #output { margin-top: 15px; }
+                </style>
+
+            </head>
+
+            <body>
+
+                <h1>Testo</h1>
+
+                <input type="text" id="textInput" placeholder="Inserisci testo..." />
+
+                <button onclick="stampa()">Stampa</button>
+
+                <div id="output"></div>
+
+                <script>
+
+                    function stampa() {
+
+                        const testo = document.getElementById('textInput').value;
+
+                        document.getElementById('output').innerText = testo;
+                    }
+                </script>
+
+            </body>
+
+        </html>
+    `);
 });
 
 const port = 5000;
